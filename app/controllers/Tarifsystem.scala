c73a6b24532c2790142d1ae8644ba79611617489
Markus Klink <justjoheinz@gmail.com>
2013-06-05 11:39:30 +0200

    added zeittarife

--- app/controllers/Tarifsystem.scala
+++ app/controllers/Tarifsystem.scala
@@ -14,6 +14,33 @@ object Tarifsystem extends Controller {
     ("verspätung_ungemeldet" -> 50.0),
     ("strafzettel" -> 5.00))
 
+  val kmTarife = Map("Kleinwagen" -> 0.13,
+    "Kompakt" -> 0.15,
+    "Komfort" -> 0.17,
+    "Extra" -> 0.19)
+
+  val zeittarife = Map(
+    "proTag" -> Map(
+      "Kleinwagen" -> 25.90,
+      "Kompakt" -> 34.90,
+      "Komfort" -> 39.90,
+      "Extra" -> 47.90),
+    "frauenNachtAuto" -> Map(
+      "Kleinwagen" -> 0.0,
+      "Kompakt" -> 0.0,
+      "Komfort" -> 0.0,
+      "Extra" -> 2.5),
+    "proWoche" -> Map(
+      "Kleinwagen" -> 149.0,
+      "Kompakt" -> 199.0,
+      "Komfort" -> 229.0,
+      "Extra" -> 279.0),
+    "proMonat" -> Map(
+      "Kleinwagen" -> 498.0,
+      "Kompakt" -> 655.00,
+      "Komfort" -> 765.0,
+      "Extra" -> 919.0))
+
   def gebuehr(art: String) = Action {
     Async {
       scala.concurrent.future {
@@ -30,7 +57,43 @@ object Tarifsystem extends Controller {
 
   }
 
-  def list() = Action {
+  def listGebuehr() = Action {
     Ok(Json.toJson(gebuehren))
+  }
+
+  def km(art: String) = Action {
+    Async {
+      scala.concurrent.future {
+        kmTarife.get(art) match {
+          case Some(betrag) => Ok(Json.parse(s"""
+	        {
+	    		"${art}": ${betrag}
+	    		}
+	        """))
+          case None => BadRequest(Json.toJson(Map("error" -> "Km-Art unbekannt")))
+        }
+      }
+    }
+
+  }
+
+  def listKm() = Action {
+    Ok(Json.toJson(kmTarife))
+  }
+
+  def zeit(art: String) = Action {
+    Async {
+      scala.concurrent.future {
+        zeittarife.get(art) match {
+          case Some(tarife) => Ok(Json.toJson(tarife))
+          case None => BadRequest(Json.toJson(Map("error" -> "Zeit-Art unbekannt")))
+        }
+      }
+    }
+
+  }
+
+  def listZeit() = Action {
+    Ok(Json.toJson(zeittarife))
   }
 }
